8b335aa55be3c3068819cd1f57d865458439c138
Mark Broihier <mbroihier@yahoo.com>
2022-05-06 21:51:35 +0000

    add utilities to send and receive files

---
+++ configure.py
@@ -0,0 +1,7 @@
+IMAP_URL = 'imaps://imap.gmail.com'
+URL = 'smtps://smtp.gmail.com:465'
+MAIL_CRED = 'agmailemailaddress@gmail.com:password'
+FROM = 'agmailemailaddress@gmail.com'
+FENDE = '/home/pi/fende/fende'
+PRIVATE_KEY_PATH = 'privatekeypath.pem'
+FILES_READ_DIRECTORY = './received_files/'
